30dd6ef4be9fa5324d0c0a196ece079dcf613da4
komissarov <komissar.off.andrey@gmail.com>
2017-12-21 00:28:30 +0300

    extra fix

--- pred10.pl
+++ pred10.pl
@@ -1,4 +1,8 @@
-% ii oi
+% oi
 subset([],[]).
 subset([X|L1],[X|L2]):-subset(L1, L2).
 subset(L1,[_|L2]):-subset(L1, L2).
+
+% ii 
+subset2([],_).
+subset2([X|L1],L2):-member(X,L2),subset2(L1,L2).
--- pred8.pl
+++ pred8.pl
@@ -1,5 +1,3 @@
 % oii ioi iii
 number(E,0,[E|_]).
-% number(_,-1,[]).
-number(E,N1,[_|T]):- number(E,N,T), N > -1, N1 is N+1. 
-number(E,N1,[_|T]):- number(E,N,T), N = -1, N1 is N.
+number(E,N1,[_|T]):- number(E,N,T), N1 is N+1. 
