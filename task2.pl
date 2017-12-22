df56c145551b9a572b54c72b568a3bfe44cd8597
komissarov <komissar.off.andrey@gmail.com>
2017-12-22 20:42:53 +0300

    several small updates (uncle,brother)

--- task2.pl
+++ task2.pl
@@ -1,74 +1,4 @@
-
-% parent oi io ii oo
-% HERE IS DATABASE +++++++
-% it should be here FIXME
-
-parent(zina,alice).
-parent(kosty,alice).
-
-parent(ira,bob).
-parent(tomas,bob).
-parent(ira,alex).
-parent(tomas,alex).
-parent(ira,kate).
-parent(tomas,kate).
-
-parent(alice,petr).
-parent(bob,petr).
-parent(alice,max).
-parent(bob,max).
-parent(alice,olga).
-parent(bob,olga).
-
-parent(eva,jane).
-parent(petr,jane).
-
-parent(marge,dave).
-parent(max,dave).
-parent(marge,kira).
-parent(max,kira).
-
-parent(olga,steve).
-parent(andrey,steve).
-parent(olga,sveta).
-parent(andrey,sveta).
-parent(olga,arnold).
-parent(andrey,arnold).
-
-
-suprug(zina,kosty).
-suprug(ira,tomas).
-suprug(alice,bob).
-suprug(eva,petr).
-suprug(marge,max).
-suprug(olga,andrey).
-
-male(max).
-male(bob).
-male(kosty).
-male(tomas).
-male(alex).
-male(petr).
-male(andrey).
-male(dave).
-male(steve).
-male(arnold).
-
-female(alice).
-female(zina).
-female(ira).
-female(kate).
-female(eva).
-female(marge).
-female(olga).
-female(jane).
-female(kira).
-female(sveta).
-
-spouses(X,Y):-suprug(X,Y).
-spouses(X,Y):-suprug(Y,Y).
-
-% HERE IS DATABASE -------
+['database.pl'].
 
 % ii io oi
 mother(X,Y):-parent(X,Y),female(X).
@@ -76,11 +6,16 @@ mother(X,Y):-parent(X,Y),female(X).
 % ii io oi
 father(X,Y):-parent(X,Y),male(X).
 
-% NEED TESTING 
-% ii io oi
+% ii io oi oo
 grandparent(X,Y):-parent(X,Z),parent(Z,Y).
 
-% NEED TESTING 
-% ii io oi
+% ii io oi oo
 grandchild(X,Y):-grandparent(Y,X).
 
+% io ii
+brother(X,Y):-parent(Z,X),parent(Z,Y),male(Y),male(Z).
+sister(X,Y):-parent(Z,X),parent(Z,Y),female(Y),female(Z).
+
+% io ii
+uncle(X,Y):-parent(Z,X),brother(Z,Y).
+aunt(X,Y):-parent(Z,X),sister(Z,Y).
